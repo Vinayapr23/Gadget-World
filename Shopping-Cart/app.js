5dc1c5d03ba8637cfd18ad5338c757c6a75b2b5c
Vinaya <vinayapr23@gmail.com>
2022-08-29 15:23:15 +0530

    Debugiing

--- Shopping-Cart/app.js
+++ Shopping-Cart/app.js
@@ -26,7 +26,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(fileUpload());
-app.use(cors)
 
 //connecting to database
 db.connect((err)=>
